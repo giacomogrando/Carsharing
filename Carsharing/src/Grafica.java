cc0c81192a4c8288d768f908f936a18ae11f1c71
franceschinnicolo <franceschinnicolo@Lab06_13.itseinaudi.local>
2016-12-20 10:30:02 +0100

    ciao

--- Carsharing/src/Grafica.java
+++ Carsharing/src/Grafica.java
@@ -1,9 +1,14 @@
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class Grafica {
 
-	protected Shell shell;
+	protected Shell shlCarsharing;
 
 	/**
 	 * Launch the application.
@@ -24,9 +29,9 @@ public class Grafica {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlCarsharing.open();
+		shlCarsharing.layout();
+		while (!shlCarsharing.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -37,10 +42,19 @@ public class Grafica {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlCarsharing = new Shell();
+		shlCarsharing.setSize(688, 507);
+		shlCarsharing.setText("Carsharing");
+		
+		Label label = new Label(shlCarsharing, SWT.SEPARATOR | SWT.HORIZONTAL);
+		label.setBounds(0, 200, 694, 10);
+		
+		List list = new List(shlCarsharing, SWT.BORDER);
+		list.setBounds(23, 10, 135, 166);
+		
+		Button btnNewButton = new Button(shlCarsharing, SWT.NONE);
+		btnNewButton.setText("ELENCO NOLLEGGI");
+		btnNewButton.setBounds(188, 54, 114, 70);
 
 	}
-
 }
